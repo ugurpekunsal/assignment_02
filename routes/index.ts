d1adc15b313167d99153cea9e34113457c96d522
Ugur Pekunsal <ugurpekunsal@gmail.com>
2021-06-04 18:53:43 -0400

    created partial content pages

--- routes/index.ts
+++ routes/index.ts
@@ -4,30 +4,30 @@ export default router;
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Home" });
+  res.render("index", { title: "Home", page: "home" });
 });
 
 /* GET home page. */
 router.get("/home", function (req, res, next) {
-  res.render("index", { title: "Home" });
+  res.render("index", { title: "Home", page: "home" });
 });
 
 /* GET about page. */
 router.get("/about", function (req, res, next) {
-  res.render("index", { title: "About" });
+  res.render("index", { title: "About Me", page: "about" });
 });
 
 /* GET projects page. */
 router.get("/projects", function (req, res, next) {
-  res.render("index", { title: "Projects" });
+  res.render("index", { title: "My Projects", page: "projects" });
 });
 
 /* GET services page. */
 router.get("/services", function (req, res, next) {
-  res.render("index", { title: "Services" });
+  res.render("index", { title: "My Services", page: "services" });
 });
 
 /* GET contact page. */
 router.get("/contact", function (req, res, next) {
-  res.render("index", { title: "Contact" });
+  res.render("index", { title: "Contact Me", page: "contact" });
 });
