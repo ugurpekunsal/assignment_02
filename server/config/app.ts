41e343e8d8afd7a2de557f6ef98c684b64aeca50
Ugur Pekunsal <ugurpekunsal@gmail.com>
2021-06-18 14:32:40 -0400

    removed type support for mongoose

--- server/config/app.ts
+++ server/config/app.ts
@@ -5,7 +5,7 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 
 import indexRouter from "../routes/index";
 
