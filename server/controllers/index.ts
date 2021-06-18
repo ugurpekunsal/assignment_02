674aa0d2f0e6236cbfd608eba558423b32f069e8
Ugur Pekunsal <ugurpekunsal@gmail.com>
2021-06-18 19:47:57 -0400

    added functions to process login and register

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -51,36 +51,102 @@ export function DisplayLoginPage(
   req: Request,
   res: Response,
   next: NextFunction
-) {
+): void {
   if (!req.user) {
-    res.render("index", {
+    return res.render("index", {
       title: "Login",
       page: "login",
       messages: req.flash("loginMessage"),
     });
   }
+
+  return res.redirect("contact-list");
 }
 
 export function ProcessLoginPage(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+): void {
+  passport.authenticate("local", (err, user, info) => {
+    // are there server errors?
+    if (err) {
+      console.error(err);
+      return next(err);
+    }
+
+    // are there login errors?
+    if (!user) {
+      req.flash("loginMessage", "Authentication Error");
+      return res.redirect("login");
+    }
+
+    req.login(user, (err) =>
+      // are there db errors?
+      {
+        if (err) {
+          console.error(err);
+          return next(err);
+        }
+
+        return res.redirect("contact-list");
+      }
+    );
+  })(req, res, next);
+}
 
 export function DisplayRegisterPage(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+): void {
+  if (!req.user) {
+    return res.render("index", {
+      title: "Register",
+      page: "register",
+      messages: req.flash("registerMessage"),
+    });
+  }
+
+  return res.redirect("contact-list");
+}
 
 export function ProcessRegisterPage(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+): void {
+  // instantiate a new User Object
+  let newUser = new User({
+    username: req.body.username,
+    emailAddress: req.body.emailAddress,
+    displayName: req.body.FirstName + " " + req.body.LastName,
+  });
+
+  User.register(newUser, req.body.password, (err) => {
+    if (err) {
+      console.error("Error: Inserting New User");
+      if (err.name == "UserExistsError") {
+        console.error("Error: User Already Exists");
+      }
+      req.flash("registerMessage", "Registration Error");
+
+      return res.redirect("register");
+    }
+
+    // after successful registration - login the user
+    return passport.authenticate("local")(req, res, () => {
+      return res.redirect("contact-list");
+    });
+  });
+}
 
 export function ProcessLogoutPage(
   req: Request,
   res: Response,
   next: NextFunction
-) {}
+): void {
+  req.logOut();
+
+  return res.redirect("login");
+}
