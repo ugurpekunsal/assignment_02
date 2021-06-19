3ef3d394c6dbf4a16e8063adbf0bc5ec8605b8dc
Ugur Pekunsal <ugurpekunsal@gmail.com>
2021-06-18 20:49:15 -0400

    added authguard to secured pages

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -7,10 +7,11 @@ const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 exports.default = router;
 const contact_1 = require("../controllers/contact");
-router.get("/", contact_1.DisplayContactListPage);
-router.get("/add", contact_1.DisplayAddPage);
-router.get("/edit/:id", contact_1.DisplayEditPage);
-router.post("/add", contact_1.ProcessAddPage);
-router.post("/edit/:id", contact_1.ProcessEditPage);
-router.get("/delete/:id", contact_1.ProcessDeletePage);
+const index_1 = require("../util/index");
+router.get("/", index_1.AuthGuard, contact_1.DisplayContactListPage);
+router.get("/add", index_1.AuthGuard, contact_1.DisplayAddPage);
+router.get("/edit/:id", index_1.AuthGuard, contact_1.DisplayEditPage);
+router.post("/add", index_1.AuthGuard, contact_1.ProcessAddPage);
+router.post("/edit/:id", index_1.AuthGuard, contact_1.ProcessEditPage);
+router.get("/delete/:id", index_1.AuthGuard, contact_1.ProcessDeletePage);
 //# sourceMappingURL=contact.js.map
--- server/routes/contact.ts
+++ server/routes/contact.ts
@@ -14,20 +14,23 @@ import {
   ProcessEditPage,
 } from "../controllers/contact";
 
+// import Util functions
+import { AuthGuard } from "../util/index";
+
 /* GET /clothing-list page. */
-router.get("/", DisplayContactListPage);
+router.get("/", AuthGuard, DisplayContactListPage);
 
 /* GET - display /clothing-list/add page. */
-router.get("/add", DisplayAddPage);
+router.get("/add", AuthGuard, DisplayAddPage);
 
 /* GET - display /clothing-list/edit/:id page. */
-router.get("/edit/:id", DisplayEditPage);
+router.get("/edit/:id", AuthGuard, DisplayEditPage);
 
 /* POST - process /clothing-list/add page */
-router.post("/add", ProcessAddPage);
+router.post("/add", AuthGuard, ProcessAddPage);
 
 /* POST - process /clothing-list/edit/:id page */
-router.post("/edit/:id", ProcessEditPage);
+router.post("/edit/:id", AuthGuard, ProcessEditPage);
 
 /* GET - process /clothing-list/delete/:id */
-router.get("/delete/:id", ProcessDeletePage);
+router.get("/delete/:id", AuthGuard, ProcessDeletePage);
